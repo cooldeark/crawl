66e3f41f26c10188864bcc70aa80a252488c1383
cooldeark <cooldeark@gmail.com>
2024-02-28 17:04:33 +0800

    fix

--- financialdata/backend/db/ptt_db_create.py
+++ financialdata/backend/db/ptt_db_create.py
@@ -29,6 +29,21 @@ for command in sql_commands:
     if command.strip() != '':
         cursor.execute(command)
 
+# 打开并读取SQL文件
+with open('/opt/crawl/financialdata/backend/db/create_partition_table.sql', 'r') as file:
+    sql_script = file.read()
+
+# 由于SQL脚本可能包含多个SQL命令，需要分割它们
+sql_commands = sql_script.split(';')
+
+# 执行每个SQL命令
+for command in sql_commands:
+    # 您可能需要检查命令是否为空字符串
+    if command.strip() != '':
+        cursor.execute(command)
+
+
+
 # 提交事务
 conn.commit()
 
