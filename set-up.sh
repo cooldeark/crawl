90eca7bcf4d54b4e438c59ee25796df9c8a2e4dc
cooldeark <cooldeark@gmail.com>
2024-02-28 15:55:58 +0800

    fix

--- set-up.sh
+++ set-up.sh
@@ -94,10 +94,10 @@ function install_python_env_params () {
 function install_python_library () {
     # python version 3.6.3
     # pyenv install miniconda3-4.3.30
-    pyenv install miniconda3-4.7.12
+    pyenv install miniconda3-4.7.10
     echo 'Finished install miniconda3'
     # pyenv global miniconda3-4.3.30
-    pyenv global miniconda3-4.7.12
+    pyenv global miniconda3-4.7.10
     echo 'Finished set global miniconda3'
     pip install pipenv
     # pipenv --python 3.8
@@ -105,7 +105,7 @@ function install_python_library () {
     pipenv install flask==2.0.1
     echo 'Finished install flask'
     # pipenv --python ~/.pyenv/versions/miniconda3-4.3.30/bin/python
-    pipenv --python ~/.pyenv/versions/miniconda3-4.7.12/bin/python
+    pipenv --python ~/.pyenv/versions/miniconda3-4.7.10/bin/python
     echo 'Finished binding path'
     pip install docker-compose pipenv
     echo 'Finished install docker compose pipenv'
