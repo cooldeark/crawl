21c209a8342c25df3ebef274358d97695fd303d0
cooldeark <cooldeark@gmail.com>
2024-03-26 20:43:51 +0800

    fix

--- apache_uvicorn.sh
+++ apache_uvicorn.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # 启动 Uvicorn 服务
-sudo pipenv run uvicorn api.main:app --host 0.0.0.0 --port 8888 &
-
-# 启动 Apache 服务
-sudo apachectl -D FOREGROUND
+pipenv run uvicorn api.main:app --host 0.0.0.0 --port 8888 & apachectl -D FOREGROUND
