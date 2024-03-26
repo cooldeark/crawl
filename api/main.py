a4b7750f6d0d3429e0c1c737cae525c85df1bada
cooldeark <cooldeark@gmail.com>
2024-03-26 11:47:34 +0800

    fix

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, engine
 from api import config
 
 
-def get_mysql_financialdata_conn() -> engine.base.Connection:
+def get_mysql_conn() -> engine.base.Connection:
     address = (
         f"mysql+pymysql://{config.MYSQL_DATA_USER}:{config.MYSQL_DATA_PASSWORD}"
         f"@{config.MYSQL_DATA_HOST}:{config.MYSQL_DATA_PORT}/{config.MYSQL_DATA_DATABASE}"
@@ -21,6 +21,26 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/ptt-line-message")
+def get_user_message():
+    # sql = f"""
+    # select * from TaiwanStockPrice
+    # where StockID = '{stock_id}'
+    # and Date>= '{start_date}'
+    # and Date<= '{end_date}'
+    # """
+    # 构建插入数据的 SQL 语句
+    sql = """
+    INSERT INTO ptt_database.lineUser (userID, crawlURL) VALUES ('test', 'test')
+    """
+    mysql_conn = get_mysql_conn()
+    # 执行 SQL 语句
+    with mysql_conn.cursor() as cursor:
+        cursor.execute(sql)
+        mysql_conn.commit()
+    # data_df = pd.read_sql(sql, con=mysql_conn)
+    return {"Hello": "World"}
+
 
 @app.get("/taiwan_stock_price")
 def taiwan_stock_price(
@@ -34,7 +54,7 @@ def taiwan_stock_price(
     and Date>= '{start_date}'
     and Date<= '{end_date}'
     """
-    mysql_conn = get_mysql_financialdata_conn()
+    mysql_conn = get_mysql_conn()
     data_df = pd.read_sql(sql, con=mysql_conn)
     data_dict = data_df.to_dict("records")
     return {"data": data_dict}
--- financialdata/backend/db/create_ptt_table.sql
+++ financialdata/backend/db/create_ptt_table.sql
@@ -14,20 +14,20 @@ CREATE DATABASE IF NOT EXISTS ptt_data;
 --     url VARCHAR(255)
 -- );
 
+CREATE TABLE `ptt_data`.`lineUser`(
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `userID` VARCHAR(255) NOT NULL,
+    `crawlURL` VARCHAR(255),
+    `crawlTitle` VARCHAR(255),
+    `createTime` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
 -- 不過還是這個萬用，可以在同一個sql檔案裡面針對不同的database作增加
-CREATE TABLE `ptt_data`.`mobiles`(
+CREATE TABLE `ptt_data`.`searchData`(
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `title` VARCHAR(255) NOT NULL,
-    `CreateTime` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    -- `TradeVolume` BIGINT NOT NULL,
-    -- `Transaction` INT NOT NULL,
-    -- `TradeValue` BIGINT NOT NULL,
-    -- `Open` FLOAT NOT NULL,
-    -- `Max` FLOAT NOT NULL,
-    -- `Min` FLOAT NOT NULL,
-    -- `Close` FLOAT NOT NULL,
-    -- `Change` FLOAT NOT NULL,
-    -- `Date` DATE NOT NULL,
-    -- PRIMARY KEY(`StockID`, `Date`)
+    `ownerID` INT NOT NULL,
+    `createTime` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
